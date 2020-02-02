7502821a85eb6d313ec6bedaa6cf0dea0f34c476
Nataly Klimova <36628483+Tataklim@users.noreply.github.com>
2020-02-02 14:39:59 +0300

    Update rle.js

--- source/rle.js
+++ source/rle.js
@@ -1,25 +1,26 @@
 'use strict';
 
-let UNICODE_SHIFT = 32;
+const UNICODE_SHIFT = 32;
 
-function createRegStr() {
+const createRegStr = () => {
     let regStr = '([A]){2,}|([a]){2,}';
 
-    for (let i = "B".charCodeAt(0); i <= "Z".charCodeAt(0); ++i) {
-        regStr += `|([${String.fromCharCode(i)}]){2,}`;
-        regStr += `|([${String.fromCharCode(i + UNICODE_SHIFT)}]){2,}`;
+    for (let i = 'B'.charCodeAt(0); i <= 'Z'.charCodeAt(0); ++i) {
+        regStr += `|([${String.fromCharCode(i)}]){2,}` +
+                  `|([${String.fromCharCode(i + UNICODE_SHIFT)}]){2,}`;
     }
     return regStr;
-}
+};
 
-function rle(input){
+const rle = (input) => {
+    if (typeof input !== 'string') {
+        return null;
+    }
 
-    let regStr = createRegStr();
+    const regStr = createRegStr();
+    const regExp = new RegExp(regStr, 'g');
 
-    let regExp = new RegExp(regStr, 'g');
-
-    return input.replace(regExp, function (match) {
+    return input.replace(regExp, (match) => {
         return match[0] + match.length;
     });
-
-}
+};
