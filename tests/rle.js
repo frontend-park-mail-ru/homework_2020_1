3f5930237099299e2b481f4d78be1fb45b2ab5a9
Nataly Klimova <36628483+Tataklim@users.noreply.github.com>
2020-02-02 11:42:00 +0300

    Update rle.js

--- tests/rle.js
+++ tests/rle.js
@@ -6,4 +6,24 @@ QUnit.module('Тестируем функцию rle', function () {
 		assert.strictEqual(rle('BCCDDDAXXXX'), 'BC2D3AX4');
 		assert.strictEqual(rle('AVVVBBBVVXDHJFFFFDDDDDDHAAAAJJJDDSLSSSDDDD'), 'AV3B3V2XDHJF4D6HA4J3D2SLS3D4');
 	});
+
+	QUnit.test('rle работает правильно на маленьких тестах', function (assert) {
+		assert.strictEqual(rle(''), '');
+		assert.strictEqual(rle('A'), 'A');
+		assert.strictEqual(rle('a'), 'a');
+		assert.strictEqual(rle('kek'), 'kek');
+		assert.strictEqual(rle('LOL'), 'LOL');
+	});
+
+	QUnit.test('rle работает правильно на тестах побольше', function (assert) {
+		assert.strictEqual(rle('LOLkek'), 'LOLkek');
+		assert.strictEqual(rle('LOOOOOOOL'), 'LO7L');
+		assert.strictEqual(rle('lOlKeK'), 'lOlKeK');
+		assert.strictEqual(rle('LLooLLKEK'), 'L2o2L2KEK');
+		assert.strictEqual(rle('QWERTYqwertyQWERTYqwerty'), 'QWERTYqwertyQWERTYqwerty');
+		assert.strictEqual(rle('AAAaaaBBbbCc'), 'A3a3B2b2Cc');
+		assert.strictEqual(rle('BCCDDDAXXXX'), 'BC2D3AX4');
+		assert.strictEqual(rle('AAAAAAAAAAaaaaaaaaaaZZZZZZZZZZZZZZZZZZZZzzzzzzzzzzzzzzzzzzzz'), 'A10a10Z20z20');
+	});
+
 });
