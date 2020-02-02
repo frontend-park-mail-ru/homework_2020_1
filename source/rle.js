37f440f8cb042ae53d857189c6a7ca5c397de149
Nataly Klimova <36628483+Tataklim@users.noreply.github.com>
2020-02-02 17:47:14 +0300

    New version

--- source/rle.js
+++ source/rle.js
@@ -1,44 +1,26 @@
 'use strict';
-
-/**
- * @description Разница между символами UNICODE 
- * @const {number}
- * @default
- */
-
-const UNICODE_SHIFT = 32;
-
-/**
- * @description Создание строки для регулярного выражения
- * @returns {string} возвращает созданную строку
- */
-
-const createRegStr = () => {
-    let regStr = '([A]){2,}|([a]){2,}';
-
-    for (let i = 'B'.charCodeAt(0); i <= 'Z'.charCodeAt(0); ++i) {
-        regStr += `|([${String.fromCharCode(i)}]){2,}` +
-                  `|([${String.fromCharCode(i + UNICODE_SHIFT)}]){2,}`;
-    }
-    return regStr;
-};
-
 /**
  * @description реализация rle-сжатия
  * @param {string} input исходная строка
- * @returns {string} возвращает результирующую сжатую строку
- * @returns {null} в случае, если на вход пришла не строка
+ * @returns {string, null} возвращает результирующую сжатую строку(output),
+ * null - в случае, если на вход пришла не строка
  */
-
 const rle = (input) => {
     if (typeof input !== 'string') {
         return null;
     }
 
-    const regStr = createRegStr();
-    const regExp = new RegExp(regStr, 'g');
+    let output = input;
+    let currentPos = 0;
 
-    return input.replace(regExp, (match) => {
-        return match[0] + match.length;
-    });
+    while (currentPos < output.length) {
+        const regStr = `([${output[currentPos]}]){2,}`;
+        const regExp = new RegExp(regStr, 'g');
+        output = output.replace(regExp, (match) => {
+            ++currentPos;
+            return match[0] + match.length;
+        });
+        ++currentPos;
+    }
+    return output;
 };
