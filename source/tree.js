473e526ff2fff8aaa2f87b2ed9bdd1f550cd614c
gavroman <gavroman1998@gmail.com>
2020-02-02 19:05:45 +0300

    Fixes after review

--- source/tree.js
+++ source/tree.js
@@ -1,16 +1,25 @@
-"use strict";
+'use strict';
 
 const tree = (height) => {
+    const type = typeof height;
+    if (type != 'number' && type != 'string') {
+        return undefined;
+    }
+    const minHeight = 3;
     height = + height;
-    if (height < 3) {
-        return null;
+    if (!Number.isInteger(height) || height < minHeight) {
+        return undefined;
     }
 
-    let levelWidth = 2 * (height - 1) - 1;
-    let resultString = "\n" + " ".repeat((levelWidth - 1) / 2) + "|" + " ".repeat((levelWidth - 1) / 2);
+    let resultString = '';
+    let levelWidth = 1;
     for (let i = 0; i != height - 1; i++) {
-        resultString += "\n" + " ".repeat(i) + "*".repeat(levelWidth) + " ".repeat(i);
-        levelWidth -= 2;
+        const spacesString = ' '.repeat(height - i - 2);
+        resultString += `${spacesString}${'*'.repeat(levelWidth)}${spacesString}\n`;
+        levelWidth += 2;
     }
-    return resultString.split("").reverse().join("");
+    const spacesString = ' '.repeat(height - 2);
+    resultString += `${spacesString}|${spacesString}\n`;
+
+    return resultString;
 }
--- tests/tree.js
+++ tests/tree.js
@@ -2,12 +2,12 @@
 
 QUnit.module('Тестируем функцию tree', function () {
     QUnit.test('Ёлочек высотой ниже трёх не бывает', function (assert) {
-        assert.strictEqual(tree(0), null);
-        assert.strictEqual(tree(1), null);
-        assert.strictEqual(tree(2), null);
-        assert.strictEqual(tree('0'), null);
-        assert.strictEqual(tree('1'), null);
-        assert.strictEqual(tree('2'), null);
+        assert.strictEqual(tree(0), undefined);
+        assert.strictEqual(tree(1), undefined);
+        assert.strictEqual(tree(2), undefined);
+        assert.strictEqual(tree('0'), undefined);
+        assert.strictEqual(tree('1'), undefined);
+        assert.strictEqual(tree('2'), undefined);
     });
 
     QUnit.test('Ёлочка высотой 3', function (assert) {
@@ -94,5 +94,24 @@ QUnit.module('Парочка дополнительных тестов функ�
             '       |       \n';
         assert.strictEqual(tree(9), expected);
         assert.strictEqual(tree('9'), expected);
+    });
+});
+
+QUnit.module('Некорректные данные', function () {
+    QUnit.test('Некорректная строка на вход', function (assert) {
+        assert.strictEqual(tree('1jfopejg'), undefined);
+        assert.strictEqual(tree('7jdihsdsfgrgb'), undefined);
+        assert.strictEqual(tree('6.6765123hjkcs'), undefined);
+        assert.strictEqual(tree('QWERTY'), undefined);
+    });
+    QUnit.test('Дробное число на вход', function (assert) {
+        assert.strictEqual(tree(4.5777), undefined);
+        assert.strictEqual(tree(3.0032), undefined);
+    });
+    QUnit.test('Объект на вход', function (assert) {
+        assert.strictEqual(tree({key : value}), undefined);
+    });
+    QUnit.test('Массив на вход', function (assert) {
+        assert.strictEqual(tree([]), undefined);
     });
 });
