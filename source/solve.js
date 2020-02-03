5a2b589cf1292a48a578295027d333716ebf039e
Deiklov <romanov408g@mail.ru>
2020-02-03 17:03:09 +0300

    Романов Андрей

---
+++ source/solve.js
@@ -0,0 +1,3 @@
+'use strict';
+
+const solve = (text, x) => eval(text)
--- tests/solve.js
+++ tests/solve.js
@@ -1,12 +1,15 @@
 'use strict';
 
 QUnit.module('Тестируем функцию solve', function () {
-	QUnit.test('solve работает правильно ', function (assert) {
-		assert.strictEqual(solve('x + 1', 1), 2);
-		assert.strictEqual(solve('2 + x - 1', 5), 6);
-		assert.strictEqual(solve('2 * x - 1', 5), 9);
-		assert.strictEqual(solve('2 * ( x - 1 )', 5), 8);
-		assert.strictEqual(solve('(5 - x) * (x + 5)', 3), 16);
-		assert.strictEqual(solve('((5 - x) * (x + 5)) * x * x', 3), 144);
-	});
+    QUnit.test('solve работает правильно ', function (assert) {
+        assert.strictEqual(solve('x + 1', 1), 2);
+        assert.strictEqual(solve('2 + x - 1', 5), 6);
+        assert.strictEqual(solve('2 * x - 1', 5), 9);
+        assert.strictEqual(solve('2 * ( x - 1 )', 5), 8);
+        assert.strictEqual(solve('(5 - x) * (x + 5)', 3), 16);
+        assert.strictEqual(solve('((5 - x) * (x + 5)) * x * x', 3), 144);
+        assert.strictEqual(solve('x*x - (1000)+((((x-8)+x*x*5*x)*2)*x)', 2), -860);
+        assert.strictEqual(solve('100-100', 30), 0);
+        assert.strictEqual(solve('(x*x+x*x-x)*(6)', -10), 1260);
+    });
 });
