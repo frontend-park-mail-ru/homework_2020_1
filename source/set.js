0fddfc2f7a85c0ddab27e51992368fe9870f511f
JettPy <smel_00@bk.ru>
2020-02-05 00:44:26 +0300

    Added jsDoc and some fixes

--- source/set.js
+++ source/set.js
@@ -1,17 +1,39 @@
 'use strict';
 
+/**
+ * устанавливает значение value в object свойству по пути path
+ *
+ * @param   {Object}   object - изменяемый объект
+ * @param   {String[]} path - путь
+ * @param   {Object}   value - значение
+ * @returns {Object} - копия измененного обЪекта
+ */
+
 function pathSet(object, path, value) {
     if (path.length === 0) {
         return object;
     }
+
+    let root = path[0];
+
     if (path.length === 1) {
-        object[path[0]] = value;
-    } else if (object[path[0]] != null) {
-        object[path[0]] = pathSet(object[path[0]], path.slice(1, path.length), value);
+        object[root] = value;
     } else {
-        object[path[0]] = pathSet({}, path.slice(1, path.length), value);
+        object[root] = pathSet(object[root] ? object[root] : {},
+                                path.slice(1, path.length), value);
     }
+
     return object;
 }
 
-const set = (object, field, value) => pathSet(object, field.split('.').slice(1, field.length), value);
+/**
+ * устанавливает значение value в object в поле field
+ *
+ * @param   {Object} object - изменяемый объект
+ * @param   {String} field - поле
+ * @param   {Object} value - значение
+ * @returns {Object} - копия измененного обЪекта
+ */
+
+const set = (object, field, value) =>
+    pathSet(object, field.split('.').slice(1, field.length), value);
