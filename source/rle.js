40fb5dd8ace92a029aaf730e56be049450fa128a
Tataklim <tatakaklim@gmail.com>
2020-02-04 11:18:54 +0300

    Replace while, changed returns value

--- source/rle.js
+++ source/rle.js
@@ -1,20 +1,18 @@
 'use strict';
-
 /**
  * @description реализация rle-сжатия
  * @param {string} input исходная строка
- * @returns {string, null} возвращает результирующую сжатую строку(output),
+ * @returns {string, undefined} возвращает результирующую сжатую строку(output),
  * null - в случае, если на вход пришла не строка
  */
 const rle = (input) => {
     if (typeof input !== 'string') {
-        return null;
+        return undefined;
     }
 
     let output = input;
-    let currentPos = 0;
 
-    while (currentPos < output.length) {
+    for (let currentPos = 0; currentPos < output.length;) {
         const regStr = `([${output[currentPos]}]){2,9}`;
         const regExp = new RegExp(regStr, 'g');
         output = output.replace(regExp, (match) => {
