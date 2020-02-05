7b5bd6d111b1b8e6373b3f1740785db3bc56c4bc
Name <Email>
2020-02-05 16:58:48 +0300

    single pass through the array

--- source/minmax.js
+++ source/minmax.js
@@ -1,6 +1,19 @@
 'use strict';
 
+function getMinMax(numbers) {
+    let result = [undefined, undefined];
+
+    numbers.forEach(el => {
+       if (el < result[0] || result[0] === undefined)
+            result[0] = el;
+       if (el > result[1] || result[1] === undefined)
+            result[1] = el;
+    });
+
+    return result;
+}
+
 const minmax = (str) => {
     const elements = str.split(/[ ,]+/).map(el => parseFloat(el)).filter(el => (isNaN(el) === false));
-    return  elements.length === 0 ? [undefined, undefined] : [Math.min(...elements), Math.max(...elements)];
+    return getMinMax(elements);
 };
