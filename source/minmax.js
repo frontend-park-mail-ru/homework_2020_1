5fd8b2ced0382ec38e8b3acb24db757ff65867ec
Name <Email>
2020-02-05 21:55:46 +0300

    Code style fixed & jsDoc added

--- source/minmax.js
+++ source/minmax.js
@@ -1,17 +1,27 @@
 'use strict';
 
+/**
+ * @description Функция нахождения максимального и минимального значения в строке
+ * @param {string} str Исходная строка
+ * @returns {Array}  Массив, первый элемент -- минимальное значение, второй -- максимальное
+ */
+
 const minmax = (str) => {
     let result = [undefined, undefined];
+    if (typeof str !== "string") return result;
 
-    if (typeof str === "string") {
-        const elements = str.split(/[ ,]+/).map(el => parseFloat(el)).filter(el => (isNaN(el) === false));
-        elements.forEach(el => {
-            if (el < result[0] || result[0] === undefined)
-                result[0] = el;
-            if (el > result[1] || result[1] === undefined)
-                result[1] = el;
-        });
-    }
+    const elements = str.split(/[ ,]+/)
+        .map(el => parseFloat(el))
+        .filter(el => (!isNaN(el)));
+
+    elements.forEach(el => {
+        if (el < result[0] || result[0] === undefined) {
+            result[0] = el;
+        }
+        if (el > result[1] || result[1] === undefined) {
+            result[1] = el;
+        }
+    });
 
     return result;
 };
