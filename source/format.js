895ab26dbb62a48abe161965fe03ce7068afc881
SPetrenko17 <SPetrenko17@yandex.ru>
2020-02-05 03:03:48 +0300

    Исправления

--- source/format.js
+++ source/format.js
@@ -1,21 +1,39 @@
 'use strict';
-const format = (data, columnsCount) => {
-    let result = '';
-    let columns = [];
-    let maxLenArr;
 
-    for (let i = 0; i < columnsCount; i++) {
-        columns[i] = [];
+/**
+ *  Функция форматирует переданные целые числа в несколько колонок.
+ *  Числа в получившейся таблице должны идти слева направо, сверху вниз.
+ *
+ * @param {number|...number} data - целое число или массив целых чисел
+ * @param {number} columnsCount - количество колонок для отформатированного вывода
+ * @returns {string|null}
+ */
+
+const format = (data, columnsCount) => {
+
+    if ('number' !== typeof data && Array.isArray(data)
+        && ('number' !== typeof columnsCount || data.some(i => !Number.isInteger(parseInt(i))))
+        || (Array.isArray(data) && data.some(i => Number(parseFloat(i)) && Number(parseFloat(i)) % 1 !== 0))) {
+        return null;
+    } else if ('number' === typeof data) {
+        return data.toString();
     }
 
+    let result = '';
+    let columns = [];
+    let maxLenArr = Array(columnsCount).fill(0);
+
     for (let i = 0, j = 0; i < data.length; i++, j++) {
+        if (columns[j] === undefined) { //инициализация массива для новой колонки
+            columns[j] = []
+        }
         if (j === columnsCount) {
             j = 0;
         }
         columns[j].push(data[i]);
+        let elementLength = data[i].toString().length;
+        maxLenArr[j] = (maxLenArr[j] < elementLength ? elementLength : maxLenArr[j]); //сохранение максимальной длины
     }
-
-    maxLenArr = getColumnsLength(...columns);
 
     for (let i = 0, x = 0, y = 0; i < data.length; i++) { // x - по столбцам, y - по строкам, i - по входным данным
         if (x === columnsCount) {
@@ -23,7 +41,7 @@ const format = (data, columnsCount) => {
             x = 0;
             result += '\n';
         }
-        result += ' '.repeat(maxLenArr[x] - columns[x][y].toString().length);
+        result += ' '.repeat(maxLenArr[x] - columns[x][y].toString().length); // выравнивание чисел в колонке
         result += columns[x++][y].toString();
         if (columnsCount > 1 && x !== columnsCount && i + 1 !== data.length) { // между колонками должен быть пробел
             result += ' ';
@@ -32,12 +50,3 @@ const format = (data, columnsCount) => {
     return result;
 };
 
-function getColumnsLength(...columns) { // находим ширину столбца для отформатированного вывода
-    let result = [];
-    for(let i = 0; i < columns.length; i++) {
-        const minLen = Math.min(...columns[i]).toString().length; // длина строки с минимальным элементом
-        const maxLen = Math.max(...columns[i]).toString().length; // длина строки с максимальным элементом
-        result[i] = (minLen > maxLen) ? minLen : maxLen;       // длина строки i-го столбца
-    }
-    return result;
-}
--- tests/format.js
+++ tests/format.js
@@ -52,4 +52,106 @@ QUnit.module('Тестируем функцию format', function () {
 		assert.strictEqual(format(input, 2), expected2);
 		assert.strictEqual(format(input, 3), expected3);
 	});
+
+	QUnit.test('format работает правильно c несколькими колонками(другой input)', function (assert) {
+		const input = [ 0, 1, 2, -5, -100000, 1020, 920, 22, 78, 0, 123456 ];
+		const expected4 =
+			'      0    1      2 -5\n' +
+			'-100000 1020    920 22\n' +
+			'     78    0 123456';
+
+		const expected5 =
+			'     0   1  2 -5 -100000\n' +
+			'  1020 920 22 78       0\n'  +
+			'123456';
+
+		const expected12 = '0 1 2 -5 -100000 1020 920 22 78 0 123456';
+
+		assert.strictEqual(format(input, 4), expected4);
+		assert.strictEqual(format(input, 5), expected5);
+		assert.strictEqual(format(input, 12), expected12);
+	});
+
+
+	QUnit.test('format работает правильно c одной колонкой, "+" и "-" числами, и числами в строках', function (assert) {
+		const input = [ 0, '1', 2, '-10', -100, 1000, '10000' ];
+		const expected =
+			'    0\n' +
+			'    1\n' +
+			'    2\n' +
+			'  -10\n' +
+			' -100\n' +
+			' 1000\n' +
+			'10000';
+		assert.strictEqual(format(input, 1), expected);
+	});
+
+	QUnit.test('format работает правильно c несколькими колонками и строками', function (assert) {
+		const input = [ 0, 1, 2, 10, 100, '-100', 1000, '10000', -10000 ];
+
+		const expected2 =
+			'     0     1\n' +
+			'     2    10\n' +
+			'   100  -100\n' +
+			'  1000 10000\n' +
+			'-10000';
+
+		const expected3 =
+			'   0     1      2\n' +
+			'  10   100   -100\n' +
+			'1000 10000 -10000';
+
+		assert.strictEqual(format(input, 2), expected2);
+		assert.strictEqual(format(input, 3), expected3);
+	});
+
+	QUnit.test('format работает правильно c одним числом', function (assert) {
+		const input = 0;
+		const expected = '0';
+		assert.strictEqual(format(input, 1), expected);
+		assert.strictEqual(format(input, 5), expected);
+	});
+
+	QUnit.test('format работает правильно c массивом из одного числа', function (assert) {
+		const input = [0];
+		const expected = '0';
+		assert.strictEqual(format(input, 1), expected);
+		assert.strictEqual(format(input, 5), expected);
+	});
+
+	QUnit.test('format работает правильно, когда количество чисел равно количеству строк (>1)', function (assert) {
+		const input = [ 0, 1, 2, 3, 4];
+		const expected = '0 1 2 3 4';
+		assert.strictEqual(format(input, 5), expected);
+	});
+
+	QUnit.test('format возвращает null, если количество колонок не является числом', function (assert) {
+		const input = [ 0, 1, 2, 3, 4];
+		const expected = null;
+		assert.strictEqual(format(input, 'Hello, World!'), expected);
+	});
+
+	QUnit.test('format возвращает null, если подаем числа с плавующей точкой', function (assert) {
+		const input = [ 0, 1.1, 2, 3, 4];
+		const expected = null;
+		assert.strictEqual(format(input, 3), expected);
+	});
+
+	QUnit.test('format возвращает null, если подается некорректный массив со строками', function (assert) {
+		const input = [ 0, 'hello', 2, 'world', 4];
+		const expected = null;
+		assert.strictEqual(format(input, 5), expected);
+	});
+
+	QUnit.test('format возвращает null, если подается некорректный массив с константами', function (assert) {
+		const input = [ 0, -Infinity, 2, Infinity, 4];
+		const expected = null;
+		assert.strictEqual(format(input, 5), expected);
+	});
+
+	QUnit.test('format возвращает null, если все данные некорректны', function (assert) {
+		const input = [ 0, 'hello', 2, '1.1', 4, 1.2, Infinity, -Infinity];
+		const expected = null;
+		assert.strictEqual(format(input, 'Hello, World!'), expected);
+	});
 });
