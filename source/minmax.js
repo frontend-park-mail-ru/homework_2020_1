78ea73aadbc09df9230dcfcbb58c161146f22c16
Name <Email>
2020-02-05 17:41:12 +0300

    into one func & tests added

--- source/minmax.js
+++ source/minmax.js
@@ -1,19 +1,17 @@
 'use strict';
 
-function getMinMax(numbers) {
+const minmax = function (str) {
     let result = [undefined, undefined];
 
-    numbers.forEach(el => {
-       if (el < result[0] || result[0] === undefined)
-            result[0] = el;
-       if (el > result[1] || result[1] === undefined)
-            result[1] = el;
-    });
-
+    if (typeof str === "string") {
+        const elements = str.split(/[ ,]+/).map(el => parseFloat(el)).filter(el => (isNaN(el) === false));
+        elements.forEach(el => {
+            if (el < result[0] || result[0] === undefined)
+                result[0] = el;
+            if (el > result[1] || result[1] === undefined)
+                result[1] = el;
+        });
+    }
+    
     return result;
-}
-
-const minmax = (str) => {
-    const elements = str.split(/[ ,]+/).map(el => parseFloat(el)).filter(el => (isNaN(el) === false));
-    return getMinMax(elements);
 };
--- tests/minmax.js
+++ tests/minmax.js
@@ -1,9 +1,22 @@
 'use strict';
 
 QUnit.module('Тестируем функцию minmax', function () {
+
+	QUnit.test('minmax работает правильно на разных входных данных', function (assert) {
+		// по условию фунция работает только со строками
+		assert.deepEqual(minmax(), [ undefined, undefined ], 'Вызов без параметров');
+		assert.deepEqual(minmax('this is a string'), [ undefined, undefined ], 'Вызов со строкой');
+		assert.deepEqual(minmax(2), [ undefined, undefined ], 'Вызов с числом');
+		assert.deepEqual(minmax(true), [ undefined, undefined ], 'Вызов с логической переменной');
+		assert.deepEqual(minmax(null), [ undefined, undefined ], 'Вызов с null');
+		assert.deepEqual(minmax(new Number(0)), [ undefined, undefined ], 'Вызов с объектом');
+		assert.deepEqual(minmax([]), [ undefined, undefined ], 'Вызов c массивом');
+	});
+
 	QUnit.test('minmax работает правильно на строках без чисел', function (assert) {
 		assert.deepEqual(minmax(''), [ undefined, undefined ], 'Особый случай, когда в строке нет чисел');
 		assert.deepEqual(minmax('мама мыла раму'), [ undefined, undefined ]);
+		assert.deepEqual(minmax('true false NaN undefined'), [ undefined, undefined ], 'Не-числовые типы');
 	});
 
 	QUnit.test('minmax правильно парсит отдельные числа', function (assert) {
@@ -19,6 +32,9 @@ QUnit.module('Тестируем функцию minmax', function () {
 		assert.deepEqual(minmax('1e5'), [ 1e5, 1e5 ]);
 		assert.deepEqual(minmax('-1e-5'), [ -1e-5, -1e-5 ]);
 		assert.deepEqual(minmax('-.1e-5'), [ -.1e-5, -.1e-5 ]);
+		assert.deepEqual(minmax('+0'), [ 0, 0 ], 'Корректная обработка знака');
+		assert.deepEqual(minmax('-0'), [ 0, 0 ], 'Корректная обработка знака');
+		assert.deepEqual(minmax('+2.3'), [ 2.3, 2.3], 'Корректная обработка знака');
 	});
 
 	QUnit.test('minmax правильно парсит несколько чисел', function (assert) {
@@ -27,6 +43,7 @@ QUnit.module('Тестируем функцию minmax', function () {
 		assert.deepEqual(minmax('1 2 3 4'), [ 1, 4 ]);
 		assert.deepEqual(minmax('-Infinity -1 0 1 Infinity'), [ -Infinity, Infinity ]);
 		assert.deepEqual(minmax('-.01 0 .01'), [ -.01, .01 ]);
+		assert.deepEqual(minmax('-1 -2.4 -3.2'), [ -3.2, -1 ], 'Все числа отрицательные');
 	});
 
 	QUnit.test('minmax игнорирует обычный текст', function (assert) {
