1742d8a1b62e1b823acbe7651861f207b77d563e
SPetrenko17 <SPetrenko17@yandex.ru>
2020-02-06 01:08:43 +0300

    Исправления[4]
    Тесты и jsDoc приведены в порядок.
    В циклах теперь есть отдельная константа с номером колонки.
    Во втором цикле используется concat без первого блока if.
    Функция возвращает result без первого символа. Использовал slice.

--- source/format.js
+++ source/format.js
@@ -6,32 +6,33 @@
  *
  * @param {number|...number} data - целое число или массив целых чисел
  * @param {number} columnsCount - количество колонок для отформатированного вывода
- * @returns {string|null}
+ * @returns {string|undefined}
  */
 
 const format = (data, columnsCount) => {
 
     if (Array.isArray(data) && (('number' !== typeof columnsCount || data.some(i => !Number.isInteger(parseInt(i))))
-        || (data.some(i => Number(parseFloat(i)) && Number(parseFloat(i)) % 1 !== 0)))) {
+        || (data.some(i => Number(parseFloat(i)) % 1 !== 0)))) {
         return undefined;
     } else if ('number' === typeof data) {
         return data.toString();
     }
 
-    let result = String();
+    let result = '';
     let columnSize = Array(columnsCount).fill(0);
 
     for (let i = 0; i < data.length; i++) {
-        columnSize[i % columnsCount] = Math.max(columnSize[i % columnsCount], data[i].toString().length);
+        const columnNumber = i % columnsCount;
+        columnSize[columnNumber] = Math.max(columnSize[columnNumber], data[i].toString().length);
     }
 
     for (let i = 0; i < data.length; i++) {
-        if (i !== 0) {
-            result += (i % columnsCount === 0) ? '\n' : ' ';
-        }
-        result += data[i].toString().padStart(columnSize[(i % columnsCount)]);
+        const columnNumber = i % columnsCount;
+        result = result.concat(((columnNumber === 0 && i !== 0) ? '\n' : ' '),
+            data[i].toString().padStart(columnSize[columnNumber]));
+
     }
 
-    return result;
+    return result.slice(1);
 };
 
