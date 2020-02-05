4b6902167d947019eb50d5aef77c8be4c8782842
Tim.Razumov <tima.razumov@gmail.com>
2020-02-05 23:20:38 +0300

    fix after 1-st review

--- source/plainify.js
+++ source/plainify.js
@@ -1,17 +1,24 @@
 'use strict';
 
+/**
+ * plainify получает на вход обычный объект в вложенными свойствами, а возвращает plain-объект
+ *
+ * @param object - обычный объект в вложенными свойствами
+ * @returns {undefined|*} - plain-объект
+ */
+
 const plainify = (object) => {
     let res = {};
-    if (typeof object != "object") {
+    if (typeof object !== 'object') {
         return res;
     }
-    for (var key in object) {
-        if (typeof object[key] != "object") {
+    for (let key in object) {
+        if (typeof object[key] !== 'object') {
            res[key] = object[key];
         } else {
-            let tmp_object = plainify(object[key]);
-            for (var tmp_key in tmp_object) {
-                res[key + "." + tmp_key] = tmp_object[tmp_key];
+            let tmpObject = plainify(object[key]);
+            for (let tmpKey in tmpObject) {
+                res[key + '.' + tmpKey] = tmpObject[tmpKey];
             }
         }
     }
