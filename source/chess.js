aa4cb8b76f6be11a0ae773dd9efb284767af2de6
GuVictory <gubanova.vi10@gmail.com>
2020-02-05 22:19:26 +0500

    inputData fixed

--- source/chess.js
+++ source/chess.js
@@ -18,13 +18,13 @@ const MIN_BOARD_SIZE = 2;
 const chess = (inputData) => {
     let output = '';
 
-    if ( isNaN(Number(inputData) ) || Number(inputData) < MIN_BOARD_SIZE) {
+    if ( isNaN(inputData = Number(inputData)) || inputData < MIN_BOARD_SIZE) {
         return undefined;
     }
 
-    const repeatSize = Math.floor(Number(inputData) / 2) + 1;
-    const positionForSlice = Number(inputData) % 2 - 2;
-    for (let i = 0; i < Number(inputData); i++) {
+    const repeatSize = Math.floor(inputData / 2) + 1;
+    const positionForSlice = inputData % 2 - 2;
+    for (let i = 0; i < inputData; i++) {
         output += (i % 2 ? ' *' : '* ').repeat(repeatSize).slice(0, positionForSlice) + '\n';
     }
 
