09066cd2cce48fe203d0d97d78cfd03540ceabf1
Alex <fakeacc666@bk.ru>
2020-02-05 20:51:51 +0300

    added comments and description of get function

--- source/get.js
+++ source/get.js
@@ -1,14 +1,25 @@
 'use strict';
 
-function get(object, properties_string) {
-    let properties = properties_string.split('.');
+
+/**
+ * This function is used to safely get object property.
+ *
+ * @param object - object you need to get property from
+ * @param property_string - same as what you would write after regular object ( e.g.".foo.bar"), but it ignores
+ * empty properties (e.g. "...foo..kek" == ".foo.kek")
+ * @returns {undefined|*} - will return property on success, will return undefined in case property can not be found
+ */
+function get(object, property_string) {
+    let properties = property_string.split('.');
 
     let result = object;
     for (let property of properties) {
+        // in case there can not be any property
         if (result === undefined || result === null) {
             return undefined;
         }
 
+        // ignore empty properties
         if (property === "") {
             continue;
         }
