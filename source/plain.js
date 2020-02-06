825c4784ada41f2d58f13d09138f2c0463be74c0
Алексей Ершков <ershkov.alex2000@gmail.com>
2020-02-06 18:16:22 +0300

    done plain function

---
+++ source/plain.js
@@ -0,0 +1,3 @@
+function plain(input_mass) {
+    return input_mass.flat(Infinity)
+}
--- tests/plain.js
+++ tests/plain.js
@@ -36,4 +36,18 @@ QUnit.module('Тестируем функцию plain', function () {
 	QUnit.test('Работает с элементами разных типов', function (assert) {
 		assert.deepEqual(plain([ [ 'abcde' ], [ [ 'f' ], [ null, false ], [ NaN, NaN ], NaN ], -Infinity ]), [ 'abcde', 'f', null, false, NaN, NaN, NaN, -Infinity ]);
 	});
+
+	QUnit.test('Работает с примитивами', function (assert) {
+		assert.deepEqual(plain([true, false]),[true,false],"Работает с boolean");
+		assert.deepEqual(plain(["hello", "world"]), ["hello", "world"], "Работает со string");
+		assert.deepEqual(plain([1,2]),[1,2],"Работает с number");
+		assert.deepEqual(plain([null]),[null], "Работает с null");
+		assert.deepEqual(plain([undefined]), [undefined], "Работает с undefined");
+		assert.deepEqual(plain([Symbol.for("id")]), [Symbol.for("id")], "Работает с symbol");
+	});
+
+	QUnit.test('Работает с объектами', function (assert) {
+		assert.deepEqual(plain([{name: "John", age:30 }]), [{name: "John", age: 30}], "Работает с одним объектом");
+		assert.deepEqual(plain([{name: "John"}, {name: "Harry"}]), [{name: "John"}, {name: "Harry"}], "Работает с несколькими объектами");
+	});
 });
