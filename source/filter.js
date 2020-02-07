59a705833148e5f90239056ced815162d5ba1c01
Andrey Prokopenko <prokopenko.rower@mail.ru>
2020-02-07 15:23:27 +0300

    fixed problem with a > bracket

--- source/filter.js
+++ source/filter.js
@@ -7,16 +7,7 @@ const filter = (htmlString, ...tags) => {
         .replace(/"/g, '&quot;')
         .replace(/'/g, '&#39;')
         .replace(/<(?!.*>)/g, '&lt;')
-        // .replace(/>/g, '&gt;');
+        .replace(/(?![^<].*>)>/g, '&gt;');
     return htmlString;
 };
-//
-// const input = '- "42!", сказала Машина. Это и был главный ответ на Вопрос жизни, вселенной & всего такого...';
-// const expected = '- &quot;42!&quot;, сказала Машина. Это и был главный ответ на Вопрос жизни, вселенной &amp; всего такого...';
-// const output = filter(input, [ 'strong', 'em' ]);
-// console.log(output);
-//
-const input = '<strong>Hello, <em>World!</em></strong> 1 + 2 < 4!';
-const output = filter(input, [ 'strong', 'em' ]);
-const expected = '<strong>Hello, <em>World!</em></strong> 1 + 2 &lt; 4!';
-console.log(output);
+
