d6a2253db5ddeed54e1a0cbd42ed9bda1a8804f5
UlianaBespalova <43138516+UlianaBespalova@users.noreply.github.com>
2020-02-07 17:13:47 +0300

    Update roman.js

--- source/roman.js
+++ source/roman.js
@@ -3,6 +3,9 @@
 const SYMBOL_ROM = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M'];
 const SYMBOL_AR = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
 
+/** @description Переводит арабские числа в римские и обратно.
+ * @param number Число в римской или арабской системе счисления.
+ */
 const roman = (number) => {
 
     if (/^[1-9]\d*$/.test(number)) {
@@ -16,6 +19,11 @@ const roman = (number) => {
     return;
 };
 
+
+/** @description Переводит римские числа в арабские.
+ * @param {string} number Число в римской системе счисления.
+ * @return {number} 
+ */
 const fromRomanToArabic = (number) => {
 
     let sym = 0;
@@ -23,19 +31,22 @@ const fromRomanToArabic = (number) => {
     let index = SYMBOL_AR.length - 1;
     number = number.toUpperCase();
 
-    while (
-        sym < number.length &&
-        ((index >= 0) || (index >= 0))    
+    while (sym < number.length &&
+	((index >= 0) || (index >= 0))    
     ) {
         if (number.substr(sym, SYMBOL_ROM[index].length) === SYMBOL_ROM[index]) {
             sym += SYMBOL_ROM[index].length;
             res += SYMBOL_AR[index];
-        } else index--;
+        } else { index--; }
     }
 
     return res;
 }
 
+/** @description Переводит арабские числа в римские.
+ * @param {number} number Число в арабской системе счисления.
+ * @return {string} 
+ */
 const fromArabicToRoman = (number) => {
 
     let res = '';
@@ -48,7 +59,7 @@ const fromArabicToRoman = (number) => {
         ) {
             res += SYMBOL_ROM[index];
             number -= SYMBOL_AR[index];
-        } else index--;
+        } else { index--; }
     }
 
     return res;
