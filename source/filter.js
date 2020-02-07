a20aa5c68b7698c52e809aed5df871cd4c841441
Andrey Prokopenko <prokopenko.rower@mail.ru>
2020-02-07 20:35:41 +0300

    added chekers for input object type

--- source/filter.js
+++ source/filter.js
@@ -9,6 +9,13 @@ const htmlEscapes = {
 };
 
 /**
+ * Get object type
+ * @param obj - object
+ * @returns {string} - result type
+ */
+const toType = obj => ({}).toString.call(obj).match(/\s([a-zA-Z]+)/)[1].toLowerCase();
+
+/**
  * Escape right brackets in string
  * @param {string} string - html string
  * @returns {string} - parsed string
@@ -36,6 +43,17 @@ const removeExtraBrackets = (string) => {
  * @returns {string} - parsed string
  */
 const filter = (htmlString, ...tags) => {
+    if (toType(htmlString) !== 'string') {
+        return '';
+    }
+    tags.forEach((set) => {
+        set.forEach((tag, index, object) => {
+            if (toType(tag) !== 'string') {
+                object.splice(index, 1);
+            }
+        });
+    });
+
     htmlString = htmlString.replace(/[&"']/g, (match, position) => {
         return htmlEscapes[match];
     }).replace(/<[^<>]+>/g, (match, position) => {
