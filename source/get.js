6586a2d977c661583c093b76d02313a3c6e20234
Shampooh <agent.serega@yandex.ru>
2020-02-07 09:32:08 +0300

    Small code style fix

--- source/get.js
+++ source/get.js
@@ -7,6 +7,6 @@ const get = (object, prop) => {
     let get_res = (p, o) =>
         p.reduce((xs, x) => (xs && xs[x]) ? xs[x] : (!x) ? o : undefined, o);
 
-    return get_res(res_prop,object);
+    return get_res(res_prop, object);
 }
 
