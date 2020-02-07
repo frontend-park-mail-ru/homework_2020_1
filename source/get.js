aa84bb20b1ae31bfecb70bedbf869d8d44ead680
Shampooh <agent.serega@yandex.ru>
2020-02-07 09:17:07 +0300

    Implement functionality of getting nested property value

---
+++ source/get.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const get = (object, prop) => {
+    let res_prop = prop.split(".");
+    res_prop.shift();
+
+    let get_res = (p, o) =>
+        p.reduce((xs, x) => (xs && xs[x]) ? xs[x] : (!x) ? o : undefined, o);
+
+    return get_res(res_prop,object);
+}
+
