2fbd911501638190b4ad06421d98575308f7199b
Andrey Prokopenko <prokopenko.rower@mail.ru>
2020-02-07 20:25:13 +0300

    added new tests

--- tests/filter.js
+++ tests/filter.js
@@ -21,7 +21,17 @@ QUnit.module('Проверка работы функции filter', function () 
 		assert.strictEqual(output, expected);
 	});
 
-	QUnit.test('filter экранирует несколько последовательных скобок', function (assert) {
+	QUnit.test('filter экранирует несколько последовательных левых скобок', function (assert) {
+		const input = '<nav> 0 / 1 << 1 / 0 </nav>';
+
+		const output = filter(input, ['nav']);
+
+		const expected = '<nav> 0 / 1 &lt;&lt; 1 / 0 </nav>';
+
+		assert.strictEqual(output, expected);
+	});
+
+	QUnit.test('filter экранирует несколько последовательных правых скобок', function (assert) {
 		const input = '<nav> 0 / 1 << 1 / 0 </nav>';
 
 		const output = filter(input, ['nav']);
@@ -34,5 +44,7 @@ QUnit.module('Проверка работы функции filter', function () 
 	QUnit.test('filter экранирует XSS', function (assert) {
 		assert.strictEqual(filter(`<script>alert('1');</script>`, [ 'strong', 'em' ]), '&lt;script&gt;alert(&#39;1&#39;);&lt;/script&gt;');
 		assert.strictEqual(filter(`<img src="bad" onerror="alert('1');">`, [ 'strong', 'em' ]), '&lt;img src=&quot;bad&quot; onerror=&quot;alert(&#39;1&#39;);&quot;&gt;');
+		assert.strictEqual(filter(`<script>alert('1');</script>`, []), '&lt;script&gt;alert(&#39;1&#39;);&lt;/script&gt;');
+
 	});
 });
