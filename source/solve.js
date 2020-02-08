166828934aa7ec9874eef07973fa0e8c68caad86
Deiklov <romanov408g@mail.ru>
2020-02-08 11:10:57 +0300

    Убрал eval, var18 Romanov

--- source/solve.js
+++ source/solve.js
@@ -1,3 +1,116 @@
 'use strict';
+/**
+ * Вычислеяет результат арифметического выржаения
+ *
+ * @param {string} text - Входная строка, например x+(x+2)*3
+ * @param {number} x - Значение входной переменной
+ * @return {number} - Вычесленный результат
+ */
+const solve = function (text, x) {
+    let str = text.replace(/[()+\-*]/g, " " + "$&" + " ").replace(/x/g, x).split(' ');
+    str = str.filter(element => element !== "");
+    const prior = {'+': 1, '-': 1, '*': 2};
+    let stackNumbers = new Stack();//стек чисел
+    let stackOperators = new Stack();//стек операций
+    for (let i = 0; i < str.length; i++) {
+        if (str[i].search(/^-?\d+$/) > -1)
+            stackNumbers.push(parseInt(str[i]));//всегда пушим числа в первый стек
+        else
+            switch (str[i]) {
+                case '(':
+                    stackOperators.push(str[i]);//всегда пушим
+                    break;
+                case ')':
+                    let topStackOperand = stackOperators.top();
+                    if (topStackOperand === '(') {
+                        stackOperators.pop();
+                        break;
+                    }//просто скипаем все
+                    else {
+                        i--;//еще раз раасматриваем скобку
+                        makeOperation(stackNumbers, stackOperators)
+                    }
+                    break;
+                case '-':
+                case '*':
+                case '+':
+                    if (stackOperators.is_empty() || stackOperators.top() === '(' || stackOperators.top() === ')' || prior[str[i]] > prior[stackOperators.top()])//сравнили приоритет нашей хрени с приоритетом верхушки стека
+                        stackOperators.push(str[i]);//пушиться все
+                    else { //тащим двух челов из стека и операцию к ним
+                        i--;
+                        makeOperation(stackNumbers, stackOperators)
+                    }
+                    break;
+                default:
+                    return "Incorrect data";
+            }
+    }
+    while (!stackOperators.is_empty())
+        makeOperation(stackNumbers, stackOperators);
+    return stackNumbers.pop();
+};
 
-const solve = (text, x) => eval(text)
+function makeOperation(stackNumbers, stackOperators) {
+    let operand1 = stackNumbers.pop();
+    let operand2 = stackNumbers.pop();
+    switch (stackOperators.pop()) {
+        case '+':
+            stackNumbers.push(operand1 + operand2);
+            break;
+        case '-':
+            stackNumbers.push(operand2 - operand1);
+            break;
+        case '*':
+            stackNumbers.push(operand1 * operand2);
+            break;
+    }
+}
+
+function Stack() {
+    this._size = 0;
+    this._storage = {};
+}
+
+/**
+ * Пушит объект в стек
+ *
+ * @param {data} data - Сам объект который хотим добавить
+ */
+Stack.prototype.push = function (data) {
+    let size = ++this._size;
+    this._storage[size] = data;
+};
+/**
+ * Возвращает объект с верхушки стека и удаляет его оттуда
+ *
+ * @return  data - Вернет верх стека
+ */
+Stack.prototype.pop = function () {
+    let size = this._size,
+        deletedData;
+
+    if (size) {
+        deletedData = this._storage[size];
+
+        delete this._storage[size];
+        this._size--;
+
+        return deletedData;
+    }
+};
+/**
+ * Возвращает объект с верхушки стека
+ *
+ * @return  data - Вернет верх стека
+ */
+Stack.prototype.top = function () {
+    return this._storage[this._size]
+};
+/**
+ * Проверят стек на пустоту
+ *
+ * @return  boolean - true если пусто, иначе false
+ */
+Stack.prototype.is_empty = function () {
+    return this._size === 0
+};
--- tests/solve.js
+++ tests/solve.js
@@ -1,15 +1,29 @@
 'use strict';
 
 QUnit.module('Тестируем функцию solve', function () {
-    QUnit.test('solve работает правильно ', function (assert) {
+    QUnit.test('Проверка простых выражений', function (assert) {
         assert.strictEqual(solve('x + 1', 1), 2);
+        assert.strictEqual(solve('100 - 100', 30), 0);
+    });
+
+    QUnit.test('Тестирование нескольких операндов', function (assert) {
         assert.strictEqual(solve('2 + x - 1', 5), 6);
         assert.strictEqual(solve('2 * x - 1', 5), 9);
+    });
+
+    QUnit.test('Тестирование со скобкам', function (assert) {
         assert.strictEqual(solve('2 * ( x - 1 )', 5), 8);
         assert.strictEqual(solve('(5 - x) * (x + 5)', 3), 16);
+    });
+
+    QUnit.test('Тестирование с вложенными скобками', function (assert) {
         assert.strictEqual(solve('((5 - x) * (x + 5)) * x * x', 3), 144);
-        assert.strictEqual(solve('x*x - (1000)+((((x-8)+x*x*5*x)*2)*x)', 2), -860);
-        assert.strictEqual(solve('100-100', 30), 0);
-        assert.strictEqual(solve('(x*x+x*x-x)*(6)', -10), 1260);
+        assert.strictEqual(solve('( x *x + x * x- x ) * (2-6)', -10), -840);
+
+    });
+    //
+    QUnit.test('Тестирование на невалидные данные', function (assert) {
+        assert.strictEqual(solve('xx+null', 2), "Incorrect data");
+        assert.strictEqual(solve('kek*89', 30), "Incorrect data");
     });
 });
