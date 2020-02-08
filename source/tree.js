dcc6d86b1be44eea577387ccc32185145dd8b09e
Oleg <elizarov1999@list.ru>
2020-02-08 19:16:51 +0300

    First try

--- source/tree.js
+++ source/tree.js
@@ -1,8 +1,17 @@
 'use strict';
 
-function tree(tr) {
-    if (tr === 0)
-    {
+function tree(lvl) {
+    if ((lvl == 0) || (lvl == 1) || (lvl == 2)) {
         return null;
     }
+    let star = 1;
+    let maxlen = 2 * (lvl - 2) + 1;
+    let derevo = ' '.repeat((maxlen - star) / 2) + '*' + ' '.repeat((maxlen - star) / 2) + '\n';
+    for (let i = 0; i < lvl - 2; i++) {
+        star = star + 2;
+        derevo = derevo + ' '.repeat((maxlen - star) / 2) + '*'.repeat(parseInt(star))
+            + ' '.repeat((maxlen - star) / 2) + '\n';
+    }
+    derevo = derevo + ' '.repeat((maxlen - 1) / 2) + '|' + ' '.repeat((maxlen - 1) / 2) + '\n';
+    return derevo;
 }
