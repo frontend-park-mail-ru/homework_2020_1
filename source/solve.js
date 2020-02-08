1811538eef8da9a6b4d51f55cbfb29837bcd9190
AliceSitedge <a.seledkina@mail.ru>
2020-02-09 01:14:49 +0300

    jsdoc

--- source/solve.js
+++ source/solve.js
@@ -4,6 +4,14 @@ const WRONG_PARAMS = 'wrong parameters';
 const INVALID_EXPR = 'invalid expression';
 
 
+/**
+ * Calculates the value of a math expression depending on the variable x.
+ * @param {String} expr - Math expression
+ * @param {Number} value - Value of the variable x
+ * @returns {Number} Calculated value
+ * @throws {TypeError} The types of the parameters are wrong.
+ * @throws {SyntaxError} The expression is invalid.
+ */
 const solve = (expr, value) => {
     if (typeof expr !== 'string' || typeof value !== 'number') {
         throw new TypeError(WRONG_PARAMS);
