f3ebf245d99685b9f8d3d80d678bb85de484b4a9
Shampooh <agent.serega@yandex.ru>
2020-02-09 04:53:53 +0300

    Add JSDoc documentation

--- source/get.js
+++ source/get.js
@@ -1,7 +1,26 @@
 'use strict';
 
+/**
+ * Get an object and path to the object's property and return its value (or undefined if property doesn't exist)
+ *
+ * @param {Object} object - An object param
+ * @param {String} prop - A path to the object's property
+ * @return {String} Object's property value
+ *
+ * @example
+ *
+ *  get({
+        foo: 'bar',
+        deep: {
+            hested: {
+                field: 'baz'
+            }
+        }
+    }, '.foo')
+ */
+
 const get = (object, prop) => {
-    let res_prop = prop.split(".");
+    let res_prop = prop.split('.');
     res_prop.shift();
 
     let get_res = (p, o) =>
