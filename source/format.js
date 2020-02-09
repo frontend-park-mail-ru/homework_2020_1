577c23bb7494ac3206a374071eefdb78d478c865
ProEugene97 <proj9007@gmail.com>
2020-02-09 11:56:33 +0300

    fix cycle

--- source/format.js
+++ source/format.js
@@ -10,35 +10,22 @@ const format = (input, collums) => {
 		return '';
 	}
 
-	const width = input.slice(0, collums)
-		.map(item => item.toString().length);
-	for (let i = 0; i < collums; ++i) {
-		for (let k = i; k < input.length; k += collums) {
-			const tmp = input[k].toString().length;
-			if (tmp >= width[i]) {
-				width[i] = tmp + (i !== 0 ? 1 : 0);
-			}
-		}
-	}
+	const isItemOfCollum = (inputIndex, collumIndex) => inputIndex % collums === collumIndex;
 
-	const indent = (item, index) => {
-		return ' '.repeat(width[index % collums] - item.toString().length);
-	};
+	const widthOfItem = (item, collumIndex) => item.toString().length + (collumIndex !== 0 ? 1 : 0);
 
-	const isEndOfLine = index => {
-		return (index + 1) % collums === 0;
-	};
+	const widthOfCollums = input.slice(0, collums)
+		.map( (_, collumIndex) => max(input.filter( (_, inputIndex) => isItemOfCollum(inputIndex, collumIndex))
+			.map( item => widthOfItem(item, collumIndex) )));
 
-	const isLast = index => {
-		return (index + 1) === input.length;
-	};
+	const indent = (item, index) => ' '.repeat(widthOfCollums[index % collums] - item.toString().length);
 
-	const charAtEndOfLine = index => {
-		return isEndOfLine(index) && !isLast(index) ? '\n' : '';
-	};
+	const isEndOfLine = index => (index + 1) % collums === 0;
+
+	const isLast = index => (index + 1) === input.length;
+
+	const charAtEndOfLine = index => isEndOfLine(index) && !isLast(index) ? '\n' : '';
 
 	return input.map( (item, index) => indent(item, index) + item + charAtEndOfLine(index))
 		.join('');
 };
-format()
-//console.log(format([], 1))
