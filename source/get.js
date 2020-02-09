53c3bd830d96ab5f5acd35f63a2839fd1529e3ca
Shampooh <agent.serega@yandex.ru>
2020-02-09 06:20:06 +0300

    Add some example regex checks for props string

--- source/get.js
+++ source/get.js
@@ -1,10 +1,24 @@
 'use strict';
 
 /**
+ * Check if first letter is '.' and next are alphabetic or numeric ones or nothing except '.'
+ *
+ * @const {[RegExp]}
+ * @todo Improve the rule (maybe not to use RegExp?)
+ */
+
+const regexArr = [
+    RegExp('[.]'),
+    RegExp('^\.\w+'),
+    RegExp('^\.\d+')
+];
+
+/**
  * Get an object and path to the object's property and return its value (or undefined if property doesn't exist)
  *
  * @author Sergey Malyshev
  * @see https://medium.com/javascript-inside/safely-accessing-deeply-nested-values-in-javascript-99bf72a0855a
+ * @see https://javascriptweblog.wordpress.com/2011/08/08/fixing-the-javascript-typeof-operator/
  * @param {Object} object - An object param
  * @param {String} prop - A path to the object's property
  * @return {String} Object's property value
@@ -21,10 +35,11 @@
     }, '.foo')
  */
 
-const get = (object, prop) => 
-    prop.split('.')
-        .slice(1)
-        .reduce((subObject, propValue) => 
-                    (subObject && subObject[propValue]) ? subObject[propValue] : (!propValue) ? object : undefined, 
-                object);
-                
+const get = (object, prop) =>
+    regexArr.some(regex => regex.test(prop)) && Object.prototype.toString.call(object) === '[object Object]' ?
+        prop.split('.')
+            .slice(1)
+            .reduce((subObject, propValue) => 
+                        (subObject && subObject[propValue]) ? subObject[propValue] : (!propValue) ? object : undefined,
+                    object)
+        : undefined;
