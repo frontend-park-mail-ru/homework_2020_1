5f14a0be5924ed5fc82b9f01a778146d79d575cb
Oleg <elizarov1999@list.ru>
2020-02-09 16:30:43 +0300

    Reduce and foreach ways for solving task

--- source/tree.js
+++ source/tree.js
@@ -1,11 +1,12 @@
+'use strict';
+
 /** @description Drawing a beautiful fir-tree.Final picture dependent on number of tree levels
- * @param {number} quantity of tree levels.
+ * @param {number} lvl- quantity of tree levels.
  * @return {string} well drawn tree that consists of star symbols and pipe symbol as trunk
  */
 
-'use strict';
-
 const tree = (lvl) => {
+
     if (!(/\d+/.test(lvl))) {
         return null
     }
@@ -16,13 +17,30 @@ const tree = (lvl) => {
 
     const MAXLENGTH = 2 * (lvl - 2) + 1;
     let star = 1;
-    let stringTree = '*'.padStart((MAXLENGTH - star) / 2 + star).padEnd((MAXLENGTH - star) + star) + '\n';
 
-    for (let i = 0; i < lvl - 2; i++) {
-        star += 2;
-        stringTree += '*'.repeat(star).padStart((MAXLENGTH - star) / 2 + star).padEnd((MAXLENGTH - star) + star) + '\n';
-    }
+    //foreach way
 
-    stringTree += '|'.padStart((MAXLENGTH - 1) / 2 + 1).padEnd((MAXLENGTH - 1) + 1) + '\n';
-    return stringTree;
+    // let treeArr = new Array( parseInt(lvl)).fill(' '.repeat(MAXLENGTH)).forEach(function (item, index, array) {
+    //     if (index === lvl - 1) {
+    //         item = item.slice(0, (MAXLENGTH - 1) / 2) + '|' + item.slice(0, (MAXLENGTH - 1) / 2);
+    //     } else {
+    //         item = item.slice(0, (MAXLENGTH - star) / 2) + '*'.repeat(star) + item.slice(0, (MAXLENGTH - star) / 2);
+    //     }
+    //     star += 2;
+    //     resTree += item.toString() + '\n';
+    // });
+    // console.log(resTree);
+    // return resTree;
+
+    //reduce way
+    return new Array(parseInt(lvl)).fill(' '.repeat(MAXLENGTH)).reduce(
+        (resTree, item, index, array) => {
+            if (index === lvl - 1) {
+                item = item.slice(0, (MAXLENGTH - 1) / 2) + '|' + item.slice(0, (MAXLENGTH - 1) / 2);
+            } else {
+                item = item.slice(0, (MAXLENGTH - star) / 2) + '*'.repeat(star) + item.slice(0, (MAXLENGTH - star) / 2);
+            }
+            star += 2;
+            return resTree + item.toString() + '\n';
+        }, '');
 };
