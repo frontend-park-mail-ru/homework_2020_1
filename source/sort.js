f55821ae52734a9bc133d707ba104172cec4c534
Alexandr Dolgavin <sanya554455@gmail.com>
2020-02-10 18:26:31 +0300

    fixed names, sortes, styles

--- source/sort.js
+++ source/sort.js
@@ -2,10 +2,10 @@
 
 const collator = new Intl.Collator();
 
-let makeNewWord = (tmpArr, i, arrLength) => {
+let makeNewWord = (tmpArr, i, strLength) => {
     tmpArr.sort(collator.compare);
     let tmpStr = tmpArr.join('');
-    if (i != arrLength) { // Пробелы после каждого слова, кроме последнего
+    if (i != strLength) { // Пробелы после каждого слова, кроме последнего
         tmpStr += " ";
     }
     tmpStr = tmpStr[0].toUpperCase() + tmpStr.slice(1).toLowerCase();
@@ -13,19 +13,19 @@ let makeNewWord = (tmpArr, i, arrLength) => {
 }
 
 
-let sort = (arr) => {
-    let newArr = "";
+let sort = (str) => {
+    let newStr = "";
     
     const tmpArr = [];
-    for (let i = 0; i <= arr.length; i++) {
-        if (arr[i] == " " || i == arr.length) {
-            newArr += makeNewWord(tmpArr, i, arr.length);
+    for (let i = 0; i <= str.length; i++) {
+        if (str[i] == " " || i == str.length) {
+            newStr += makeNewWord(tmpArr, i, str.length);
             tmpArr.length = 0;
             continue; // Случай " " не записываем в tmpArr
         }
-        tmpArr.push(arr[i]);
+        tmpArr.push(str[i]);
     }
-    const result = newArr.split(' ');
+    const result = newStr.split(' ');
     result.sort(collator.compare);
     return result.join(' ');
 };
