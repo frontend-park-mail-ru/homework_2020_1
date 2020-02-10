9a35f151b907331051cead1444bde27ef803315a
Egor Bedov <bedovegor2203@gmail.com>
2020-02-11 00:05:05 +0300

    Small fixes #2

--- source/inverse.js
+++ source/inverse.js
@@ -12,22 +12,16 @@
 const TYPE_ERROR = 'invalid arguments';
 
 const inverse = (array, index = 0) => {
-    if (!Array.isArray(array) || typeof index != 'number' ) {
-        return TYPE_ERROR
+    if (!Array.isArray(array) || typeof index !== 'number' ) {
+        return TYPE_ERROR;
     }
 
     if (Math.abs(index) >= array.length || array.length === 0) {
         return array;
     }
 
-    let start, length;
-    if (index <= 0) {
-        start = 0;
-        length = index;
-    } else {
-        start = index;
-        length = 0;
-    }
+    const [start, length] = index <= 0 ? [0, index] : [index, 0];
+
     const reversedPart = array.slice(start, array.length + length).reverse();
     array.splice(start, reversedPart.length, ...reversedPart);
     return array;
