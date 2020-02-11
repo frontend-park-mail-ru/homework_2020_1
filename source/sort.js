0eed39e6615d3e07861b499ff024b5140c602416
Alexandr Dolgavin <sanya554455@gmail.com>
2020-02-11 21:53:45 +0300

    fixed variables, changed to map

--- source/sort.js
+++ source/sort.js
@@ -2,30 +2,18 @@
 
 const collator = new Intl.Collator();
 
-let makeNewWord = (tmpArr, i, strLength) => {
-    tmpArr.sort(collator.compare);
-    let tmpStr = tmpArr.join('');
-    if (i != strLength) { // Пробелы после каждого слова, кроме последнего
-        tmpStr += " ";
-    }
+
+const makeNewWord = (currentValue) => {
+    const arrayOfSymbols = currentValue.split('');
+    arrayOfSymbols.sort(collator.compare);
+    let tmpStr = arrayOfSymbols.join('');
     tmpStr = tmpStr[0].toUpperCase() + tmpStr.slice(1).toLowerCase();
     return tmpStr;
 }
 
 
-let sort = (str) => {
-    let newStr = "";
-    
-    const tmpArr = [];
-    for (let i = 0; i <= str.length; i++) {
-        if (str[i] == " " || i == str.length) {
-            newStr += makeNewWord(tmpArr, i, str.length);
-            tmpArr.length = 0;
-            continue; // Случай " " не записываем в tmpArr
-        }
-        tmpArr.push(str[i]);
-    }
-    const result = newStr.split(' ');
-    result.sort(collator.compare);
-    return result.join(' ');
+const sort = (str) => {
+    const newArr = str.split(' ').map(makeNewWord);
+    newArr.sort(collator.compare);
+    return(newArr.join(' '));
 };
