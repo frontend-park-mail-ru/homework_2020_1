59eb25073e0a7eaf8757463856806f4802d6e308
Alexandr Dolgavin <sanya554455@gmail.com>
2020-02-12 13:57:51 +0300

    added docs and fixed names of variables

--- source/sort.js
+++ source/sort.js
@@ -2,15 +2,28 @@
 
 const collator = new Intl.Collator();
 
+/**
+ * Создаем новое слово и сортируем символы внутри него
+ * 
+ * @param {string} word - слово из массива str
+ * @returns {string}
+ */
 
-const makeNewWord = (currentValue) => {
-    const arrayOfSymbols = currentValue.split('');
-    arrayOfSymbols.sort(collator.compare);
-    let tmpStr = arrayOfSymbols.join('');
+const makeNewWord = (word) => {
+    const symbols = word.split('');
+    symbols.sort(collator.compare);
+    let tmpStr = symbols.join('');
     tmpStr = tmpStr[0].toUpperCase() + tmpStr.slice(1).toLowerCase();
     return tmpStr;
 }
 
+/**
+ * Сортируем буквы в словах по алфавиту, а потом получившиеся слова в предложении - тоже.
+ * Первая буква каждого слова делается прописной, остальные строчными.
+ * 
+ * @param {string} str - входная строка слов
+ * @returns {string}
+ */
 
 const sort = (str) => {
     const newArr = str.split(' ').map(makeNewWord);
