8c9636952817a3f15572e8ea27019c5c1d86ba1c
Shampooh <agent.serega@yandex.ru>
2020-02-13 18:25:43 +0300

    Fix code styles and improve code readability

--- source/get.js
+++ source/get.js
@@ -1,7 +1,36 @@
 'use strict';
 
 /**
- * Get an object and path to the object's property and return its value (or undefined if property doesn't exist)
+ * Get an object and path to the object's property and make some prechecks
+ * to be sure in object and path states and their correctness
+ *
+ * @author Sergey Malyshev
+ * @see https://stackoverflow.com/a/32108184
+ * @param {object} object - An object param
+ * @param {string} objectPropPath - A path to the object's property
+ * @returns {boolean} Boolean state of object and path
+ *
+ * @example
+ *
+ *  inputPreCheck({
+        foo: 'bar',
+        deep: {
+            hested: {
+                field: 'baz'
+            }
+        }
+    }, '.foo')
+ */
+
+const inputPreCheck = (object, objectPropPath) => {
+    return typeof objectPropPath === 'string' &&
+    objectPropPath.startsWith('.') &&
+    object && object.constructor === Object;
+}
+
+/**
+ * Get an object and path to the object's property and return its value
+ * (or undefined if property doesn't exist)
  *
  * @author Sergey Malyshev
  * @see https://medium.com/javascript-inside/safely-accessing-deeply-nested-values-in-javascript-99bf72a0855a
@@ -23,18 +52,16 @@
  */
 
 const get = (object, objectPropPath) =>
-    typeof objectPropPath === 'string' && objectPropPath.startsWith('.') && object && object.constructor === Object ?
+    inputPreCheck(object, objectPropPath) ?
         objectPropPath.split('.')
             .slice(1)
             .reduce((nestedObject, propPath) => {
                  if (nestedObject) {
                     switch (true) {
-                        case propPath==='':
+                        case propPath === '':
                             return object;
                         case (nestedObject.constructor === Object):
-                            return nestedObject[propPath];
                         case (nestedObject.constructor === Array):
-                            return nestedObject[propPath];
                         case (nestedObject.constructor === String):
                             return nestedObject[propPath];
                     }
