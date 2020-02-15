7c9f12b43f5e6c4cb53f2e48d6d389128cb9d85a
Shampooh <agent.serega@yandex.ru>
2020-02-15 10:33:45 +0300

    Huge improve in the implemented solution, thx to @Drujininii

--- source/get.js
+++ source/get.js
@@ -1,5 +1,11 @@
 'use strict';
 
+const OBJECT_TYPES = [
+    'object',
+    'array',
+    'string'
+];
+
 /**
  * Provide access to undefiened value
  *
@@ -17,7 +23,7 @@ const noop = () => void 0;
  * to be sure in object and path states and their correctness
  *
  * @author Sergey Malyshev
- * @see https://stackoverflow.com/a/32108184
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Nullish_coalescing_operator
  * @param {object} object - An object param
  * @param {string} objectPropPath - A path to the object's property
  * @returns {boolean} Boolean state of object and path
@@ -35,9 +41,37 @@ const noop = () => void 0;
  */
 
 const isCorrectInput = (object, objectPropPath) => {
-    return typeof objectPropPath === 'string' &&
-    objectPropPath.startsWith('.') &&
-    object && object.constructor === Object;
+    return (object ?? false) && 
+                OBJECT_TYPES.some(objectType => typeof object === objectType) &&
+                typeof objectPropPath === 'string' &&
+                objectPropPath.startsWith('.');
+}
+
+/**
+ * Get a nested object and key to the object's property and make some prechecks
+ * to be sure in object and property existing and their correctness
+ *
+ * @author Sergey Malyshev
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Nullish_coalescing_operator
+ * @param {object} nestedObject - An object param
+ * @param {string} objectProp - A key to the object's property
+ * @returns {boolean} Boolean state of object and path
+ *
+ * @example
+ *
+ *  isCorrectNest({
+        foo: 'bar',
+        deep: {
+            hested: {
+                field: 'baz'
+            }
+        }
+    }, '.foo')
+ */
+const isCorrectNest = (nestedObject, objectProp) => {
+    return (nestedObject ?? false) &&
+            OBJECT_TYPES.some(objectType => typeof nestedObject === objectType) && 
+            nestedObject.hasOwnProperty(objectProp);
 }
 
 /**
@@ -45,8 +79,6 @@ const isCorrectInput = (object, objectPropPath) => {
  * (or undefined if property doesn't exist)
  *
  * @author Sergey Malyshev
- * @see https://medium.com/javascript-inside/safely-accessing-deeply-nested-values-in-javascript-99bf72a0855a
- * @see https://javascriptweblog.wordpress.com/2011/08/08/fixing-the-javascript-typeof-operator/
  * @param {object} object - An object param
  * @param {string} objectPropPath - A path to the object's property
  * @returns {(string|undefined)} Object's property value
@@ -70,15 +102,13 @@ const get = (object, objectPropPath) => {
     return objectPropPath.split('.')
         .slice(1)
         .reduce((nestedObject, propPath) => {
-                if (nestedObject) {
                 switch (true) {
-                    case propPath === '':
+                    case (propPath === ''):
                         return object;
-                    case (nestedObject.constructor === Object):
-                    case (nestedObject.constructor === Array):
-                    case (nestedObject.constructor === String):
+                    case (isCorrectNest(nestedObject, propPath)):
                         return nestedObject[propPath];
+                    default:
+                        return noop();
                 }
-            }
         }, object);
 }
